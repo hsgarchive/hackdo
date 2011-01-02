a507017220f0c87a0601edbd18d8ecd0162756a2
Ruiwen Chua <ruiwen@hackerspace.sg>
2011-01-02 20:52:25 +0800

    Shifting admin urls to the top so that they get trapped first, ahead of the catch-all clause for the hado app

--- urls.py
+++ urls.py
@@ -10,12 +10,14 @@ ROOT_PATH = os.path.dirname(__file__) # Handy for dynamic paths across Dev and P
 urlpatterns = patterns('',
 	# Example:
 	# (r'^hackdo/', include('hackdo.foo.urls')),
+
+	# Uncomment the next line to enable the admin:
+	(r'^admin/', include(admin.site.urls)),
+
 	
 	(r'', include('hado.urls')),
 	# Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
 	# to INSTALLED_APPS to enable admin documentation:
 	# (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-	# Uncomment the next line to enable the admin:
-	(r'^admin/', include(admin.site.urls)),
 )
