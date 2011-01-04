d4c47506945b3cffd06a08a96423f09f82f579dc
Ruiwen Chua <ruiwen@hackerspace.sg>
2011-01-05 02:11:51 +0800

    Defining new custom AdminSite
    A custom AdminSite will allow us to extend the Django admin interface,
    plugging in new displays, turning it into our very own dashbaord.

---
+++ hado/admin_site.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import datetime
+
+from django.contrib.admin.sites import AdminSite
+from django.db.models import Q
+from hado.models import *
+from utils import render
+
+# Subclassing a custom admin to provide a custom interface
+class HackdoAdmin(AdminSite):
+
+	def index(self, request):
+		'''This view forms the admin dashboard for HackDo'''
+		
+		# Members stats
+		members = {}
+		users = User.objects.select_related('contracts')
+		members['total'] = len(users)
+
+		members['active'] = Contract.objects.filter(ctype__desc='Membership', status='ACT').count()
+		members['lapsed'] = Contract.objects.filter(ctype__desc='Membership', status='LAP').count()
+
+		# Income stats
+		income = {}
+		this_month = datetime.date.today().month
+		payments_this_month = Payment.objects.filter(date_paid__month=this_month)
+		# Get ContracTypes
+		ctypes = ContractType.objects.all()		
+		for c in ctypes:
+			income[c.desc] = payments_this_month.filter(contract__ctype__desc=c.desc).aggregate(Sum('amount'))['amount__sum'] or 0.0
+		
+		
+		# Supposed income this month
+		income['summary'] = {}
+		income['summary']['supposed'] = Contract.objects.filter(Q(status='ACT') | Q(status='LAP')).aggregate(Sum('tier__fee'))['tier__fee__sum'] or 0.0
+		income['summary']['actual'] = payments_this_month.aggregate(Sum('amount'))['amount__sum'] or 0.0
+		income['summary']['shortfall'] = income['summary']['supposed'] - income['summary']['actual']
+		
+		return render(request, 'admin/index.html', {'members':members, 'income':income})
+
+
+
+	def lapsed_contracts(self, request):
+		'''Shows details about lapsed contracts'''
+		pass
+		
+	
+	
