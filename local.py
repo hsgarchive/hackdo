5bc3a4957948d77c73a9a75c8eed92c40a09ee19
Ruiwen Chua <ruiwen@hackerspace.sg>
2011-09-16 03:19:56 +0800

    Adding TEST_NAME to database settings as name of test database to use. We use 'hackdo_test' so we can simply grant permissions on 'hackdo_*' for our user

--- local.py
+++ local.py
@@ -8,6 +8,7 @@ DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 		'NAME': 'hackdo',						# Or path to database file if using sqlite3.
+		'TEST_NAME': 'hackdo_test',
 		'USER': 'hackdo',						# Not used with sqlite3.
 		'PASSWORD': 'hackdo',				  # Not used with sqlite3.
 		'HOST': '',						 # Set to empty string for localhost. Not used with sqlite3.
