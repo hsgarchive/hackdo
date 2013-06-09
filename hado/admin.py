3b4fe1f059e88e48cc7d1f220a71ddd61d3513c6
wgx731 <wgx731@gmail.com>
2013-06-09 23:21:14 +0800

    Fix bug in user creation form
    bug fix
    Create own user creation form, default user creation from will
    use auth_user table.

--- hado/admin.py
+++ hado/admin.py
@@ -4,9 +4,7 @@ import re
 from django.utils.translation import ugettext_lazy as _
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.forms import (
-    UserCreationForm, UserChangeForm
-)
+from django.contrib.auth.forms import UserChangeForm
 from django import forms
 
 import datetime
@@ -85,26 +83,80 @@ class ContractAdmin(admin.ModelAdmin):
     inlines = [ PaymentInline, ]
 
 
-class HackDoUserCreationForm(UserCreationForm):
+class HackDoUserCreationForm(forms.ModelForm):
+    error_messages = {
+        'duplicate_username': _("A user with that username already exists."),
+        'password_mismatch': _("The two password fields didn't match."),
+    }
+    username = forms.RegexField(label=_("Username"), max_length=40,
+                                regex=r'^[\w.@+-]+$',
+                                help_text=_("Required. 40 characters or fewer. Letters, digits and "
+                                            "@/./+/-/_ only."),
+                                error_messages={
+                                    'invalid': _("This value may contain only letters, numbers and "
+                                                 "@/./+/-/_ characters.")})
+    password1 = forms.CharField(label=_("Password"),
+                                widget=forms.PasswordInput)
+    password2 = forms.CharField(label=_("Password confirmation"),
+                                widget=forms.PasswordInput,
+                                help_text=_("Enter the same password as above, for verification."))
+
+    def clean_username(self):
+        # Since HackDoUser.username is unique, this check is redundant,
+        # but it sets a nicer error message than the ORM. See #13147.
+        username = self.cleaned_data["username"]
+        try:
+            HackDoUser._default_manager.get(username=username)
+        except HackDoUser.DoesNotExist:
+            return username
+        raise forms.ValidationError(self.error_messages['duplicate_username'])
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                self.error_messages['password_mismatch'])
+        return password2
+
+    def save(self, commit=True):
+        user = super(HackDoUserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
 
     class Meta:
         model = HackDoUser
-        fields = ('username', 'email', )
+        fields = ('username', 'email',)
 
 class HackDoUserChangeForm(UserChangeForm):
+    username = forms.RegexField(
+        label=_("Username"), max_length=40, regex=r"^[\w.@+-]+$",
+        help_text=_("Required. 40 characters or fewer. Letters, digits and "
+                    "@/./+/-/_ only."),
+        error_messages={
+            'invalid': _("This value may contain only letters, numbers and "
+                         "@/./+/-/_ characters.")})
 
     class Meta:
         # TODO: Find a better way to add all required fields into the form
         model = HackDoUser
         fields = (
-            'username', 'email', 'is_active', 
+            'username', 'email', 'is_active',
             'first_name', 'last_name', 'groups',
             'is_staff', 'is_superuser', 'is_hackdo_admin',
             'user_permissions', 'last_login', 'date_joined',
         )
 
 class HackDoUserAdmin(UserAdmin):
-    form = HackDoUserChangeForm
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2')}
+        ),
+    )
+    add_form = HackDoUserCreationForm
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
         (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
@@ -112,12 +164,9 @@ class HackDoUserAdmin(UserAdmin):
                                        'is_hackdo_admin', 'groups', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('username', 'email', 'password1', 'password2')}
-        ),
-    )
+    form = HackDoUserChangeForm
+    list_display = ('username', 'email', 'is_staff', 'is_superuser', 'is_active', 'is_hackdo_admin')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'is_hackdo_admin')
     inlines = [ ContractInline, PaymentInline ]
 
 
