4ac7f8441048c060462c4d30481bbe91b925f840
wgx731 <wgx731@gmail.com>
2013-06-23 20:46:01 +0800

    Change sql script
    SQL
    Drop before create
    Add warning for user to change password before use it
    [ci skip]

--- scripts/hackdo.sql
+++ scripts/hackdo.sql
@@ -1,7 +1,12 @@
-# MySQL script to create the user and database needed to deploy the application
+-- MySQL script to create the user and database needed to deploy the application
 
+DROP DATABASE IF EXISTS hackdo;
+DROP DATABASE IF EXISTS hackdo_test;
+CREATE DATABASE hackdo COLLATE utf8_general_ci;
+CREATE DATABASE hackdo_test COLLATE utf8_general_ci;
 
-CREATE USER 'hackdo'@'localhost' IDENTIFIED BY 'hackdo';
-CREATE DATABASE IF NOT EXISTS hackdo COLLATE utf8_general_ci;
-GRANT ALL ON hackdo.* TO 'hackdo'@'localhost';
+-- Change password before run this script
+GRANT ALL ON hackdo.* TO 'hackdo'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL ON hackdo_test.* TO 'hackdo'@'localhost';
 GRANT ALL PRIVILEGES ON  `hackdo\_%` . * TO  'hackdo'@'localhost';
+GRANT ALL PRIVILEGES ON  `hackdo_test\_%` . * TO  'hackdo'@'localhost';
