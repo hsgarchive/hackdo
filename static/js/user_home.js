ae14d0abca60ef1a048ecea2cc7b6d55a3c56d0d
wgx731 <wgx731@gmail.com>
2013-06-30 23:30:31 +0800

    Use compressed css and javascript
    compression
    [ci skip]

--- static/js/user_home.js
+++ static/js/user_home.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 
+    //To start datepicker
+    $('.datepicker').datetimepicker({
+        pickTime: false
+    });
+
     var csrftoken = $.cookie('csrftoken');
     /*
      * Builds AJAX URL
