e67ee309085b6d9e324467cc9ec3c37265806bbb
wgx731 <wgx731@gmail.com>
2013-07-29 18:24:25 +0800

    Redirect application admin to admin index
    [ci skip]

--- urls.py
+++ urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, patterns
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 from django.contrib import admin
 admin.autodiscover()
@@ -11,6 +13,8 @@ urlpatterns = patterns(
     '',
     (r'^amedia/jsi18n', 'django.views.i18n.javascript_catalog'),
     (r'^hdadmin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^hdadmin/hado/$',
+        lambda x: HttpResponseRedirect(reverse('admin:index'))),
     (r'^hdadmin/', include(hdadmin.urls)),
     (r'', include('hado.urls')),
     (r'^static/(?P<path>.*)$',
